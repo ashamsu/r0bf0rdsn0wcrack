7dd32943361203c52261e29b2c80ec2206cc16d3
spawncpy <spawncpy@gmail.com>
2014-09-21 18:32:37 +0200

    LOLOLOOL
    ./ for python
    LOLOLOLOLOLOL

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /r0bf0rdsn0wcrack/
-offset1 = ./getkbagloc.py
-offset2 = ./getaesloc.py
+offset1 = python getkbagloc.py
+offset2 = python getaesloc.py
 echo "got dem offsets"
 ./iBootPayload offset1 offset2
