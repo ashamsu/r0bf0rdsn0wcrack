7d204d5c7c4ef6b50d746f6994695a428fdc97ef
spawncpy <spawncpy@gmail.com>
2014-09-21 18:26:39 +0200

    lol
    forgot to flip bytes lol

--- getaesloc.py
+++ getaesloc.py
@@ -1,6 +1,6 @@
 def grabAESOffset(file):
    s = ConstBitStream(file)
-   found = s.find('/x61/x65/x73/x5f/x68/x77/x5f/x63/x72/x79/x70/x74/x6f/x5f/x63/x6d/x64/x5f/x74', bytealigned=False)
+   found = s.find('6165735f68775f63727970746f5f636d645f74', bytealigned=False) 
    print "aes_hw_crypto_cmd_t location found at %d" %found
 
 def main():
--- getkbagloc.py
+++ getkbagloc.py
@@ -1,6 +1,6 @@
 def grabKernelKBAG(file):
    s = ConstBitStream(file)
-   found = s.find('/x00/x00/x47/x41/x42/x4B', bytealigned=False)
+   found = s.find('00004741424B', bytealigned=False)
    print "KBAG location found at %d" %found
 
 def main():
